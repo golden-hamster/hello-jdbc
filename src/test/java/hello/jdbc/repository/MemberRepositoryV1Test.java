8631f957129f5648c23935576e4eb773dc6f3f6e
Isak Jung <isack19999@gmail.com>
2023-04-22 23:58:41 +0900

    hello-jdbc

--- src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -24,7 +24,7 @@ class MemberRepositoryV1Test {
     @BeforeEach
     void beforeEach() {
         //기본 DriverManager - 항상 새로운 커넥션 획득
-//       DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+//      DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
 
         //커넥션 풀링
         HikariDataSource dataSource = new HikariDataSource();
